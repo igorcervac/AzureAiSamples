501fc84a78d3051654f6a096dbc39093a1ff7348
igorcervac <igor.cervac@outlook.com>
2025-01-12 19:06:48 -0500

    Add setup.bat

---
+++ SummarizeText/setup.bat
@@ -0,0 +1,14 @@
+set "group=az-ai-snippets-textanalytics-rg"
+set "name=az-ai-snippets-textanalytics"
+set "location=eastus"
+set "kind=TextAnalytics"
+set "sku=F0"
+
+call az group create --name %group% --location %location%
+call az cognitiveservices account create --name %name% --resource-group %group% --sku %sku% --kind %kind% --location %location%
+call az cognitiveservices account keys list --name %name% --resource-group %group% --output table
+call az cognitiveservices account show  --name %name% --resource-group %group% --query "properties.endpoint"
+
+@REM call az cognitiveservices account delete --name %name% --resource-group %group%
+@REM call az cognitiveservices account purge --name %name% --resource-group %group% --location %location%
+@REM call az group delete --name %group% --no-wait --yes
